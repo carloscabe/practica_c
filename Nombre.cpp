e423f5db9175253f6cecb6c2378699f00bc3cebf
carloscabe <155786810+carloscabe@users.noreply.github.com>
2024-01-12 20:53:16 -0400

    Add files via upload

---
+++ Nombre.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	//DECLARACION DE UNA CADENA DE CARACTERES
+	char nombre[]="CARLOS";
+	
+	cout<<"imprimiendo la NOMBRE '\':"<<nombre<<endl;
+	cout<<"imprimiendo nombre [0] '\':"<<nombre[0]<<endl;
+	cout<<"imprimiendo nombre [1] '\':"<<nombre[1]<<endl;
+	cout<<"imprimiendo nombre [2] '\':"<<nombre[2]<<endl;
+	cout<<"imprimiendo nombre [3] '\':"<<nombre[3]<<endl;
+	cout<<"imprimiendo nombre [4] '\':"<<nombre[4]<<endl;
+	cout<<"imprimiendo nombre [5] '\':"<<nombre[5]<<endl;
+	nombre[0]='c';
+	nombre[1]='a';
+	nombre[2]='r';
+	nombre[3]='l';
+	nombre[4]='o';
+	nombre[5]='s';
+	cout<<"imprimiendo la NOMBRE '\':"<<nombre<<endl;
+	
+	return 0;
+}
+
---
+++ booleanos.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char*argv[]) {
+	// Declaracion y uso del bool
+	bool b0;
+	b0=true;
+	cout<<"variable con valor VERDADERO:"<<b0<<endl;
+	b0=false;
+	cout<<"variable con valor FALSO:"<<b0<<endl;
+	//Asignacion de valores numericos
+	//si es cero es FALSO, si es diferente de cero es verdadero.
+	b0=0; //El valor se vera con false 
+	cout<<"variable con valor 0:" <<b0<<endl;
+	b0=1;//El valor se vera como true .
+	cout<<"variable con valor 1:"<<b0<<endl;
+	b0=-100;//E el valor se vera como true
+	cout<<"variable con valor -100:"<<b0<<endl;
+	return 0;
+	
+}

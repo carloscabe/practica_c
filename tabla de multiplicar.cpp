c37a9f70a05c6a7a9d705567bb2cfd848ef787ab
carloscabe <155786810+carloscabe@users.noreply.github.com>
2024-01-17 21:38:44 -0400

    tabla de multiplicar.cpp

--- tabla de multiplicar.cpp
+++ tabla de multiplicar.cpp
@@ -1,16 +1,25 @@
+/*
+Titulo: Tabla de Multiplicacion. 
+Autor: Carlos eduardo cabello cabello
+Descripcion: Escribir un programa que imprima la tabla de multiplicar de un numero ingresado por el usuario. 
+Fecha: 17/01/2024
+
+*/
+
+
 #include <iostream>
 using namespace std;
 
 int main(int argc, char *argv[]) {
 	
-	// Variable para almacenar el número ingresado por el usuario
+	// Variable para almacenar el nÃºmero ingresado por el usuario
 	int numero;
 	
-	// Solicitar al usuario que ingrese un número
+	// Solicitar al usuario que ingrese un nÃºmero
 	std::cout << "Ingrese el numero para realizar la tabla: ";
 	std::cin >> numero;
 	
-	// Mostrar el número ingresado por el usuario
+	// Mostrar el nÃºmero ingresado por el usuario
 	std::cout << "\nTabla de multiplicar del " << numero << ":\n";
 	
 	// Bucle FOR para imprimir la tabla de multiplicar

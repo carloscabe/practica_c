cdcfb7155202f2b480b47890f98a437410d87224
carloscabe <155786810+carloscabe@users.noreply.github.com>
2024-01-17 21:34:00 -0400

    Add files via upload

---
+++ generador de numeros.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cstdlib> // se necesita para la función rand()
+#include <ctime>   // se necesita para la función time()
+
+// Utilizamos el espacio de nombres std para evitar la repetición de std::
+using namespace std;
+
+int main() {
+	int cantidad;
+	
+	// se solicita al usuario ingresar que cantidad de numeros quiere generar.
+	cout << "Ejercicio 2: Generación de NUmeros Aleatorios\n";
+	cout << "¿Cuántos números quiere generar? ";
+	cin >> cantidad;
+	
+	// Iniciar la semilla para obtener números realmente aleatorios
+	srand(time(0));
+	
+	// imprimir los numeros aleatorios que se generaron
+	cout << "Números aleatorios generados: ";
+	for (int i = 0; i < cantidad; ++i) {
+		// generar numeros aleatorios y mostrarlos
+		cout << rand() % 100;
+		// espaciado entre numeros.
+		if (i < cantidad - 1) {
+			cout << ", ";
+		}
+	}
+	cout << endl;
+	
+	return 0;
+}
---
+++ tabla de multiplicar.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	
+	// Variable para almacenar el número ingresado por el usuario
+	int numero;
+	
+	// Solicitar al usuario que ingrese un número
+	std::cout << "Ingrese el numero para realizar la tabla: ";
+	std::cin >> numero;
+	
+	// Mostrar el número ingresado por el usuario
+	std::cout << "\nTabla de multiplicar del " << numero << ":\n";
+	
+	// Bucle FOR para imprimir la tabla de multiplicar
+	for (int i = 1; i <= 10; ++i) {
+		std::cout << numero << " x " << i << " = " << (numero * i) << std::endl;
+	}
+	return 0;
+}
+

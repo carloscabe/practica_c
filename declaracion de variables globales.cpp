1100d2f8587de489459b59173e843bb8b2210e68
carloscabe <155786810+carloscabe@users.noreply.github.com>
2024-01-24 19:28:15 -0400

    Add files via upload

---
+++ declaracion de variables globales.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+	//Declaracion de variables globales.
+	int n1,n2;
+	
+	int sumar()
+	{
+		return n1+n2;
+	}
+	
+int multiplicar()
+{
+	return n1*n2;
+}
+void ingresar()
+{
+	cout <<"ingresar el primer numero:";
+	cin >>n1;
+	cout <<"ingresar el segundo numero:";
+	cin >>n2;
+	
+}	
+
+int main(int argc, char *argv[])  {
+	ingresar ();
+	cout <<"suma de los numeros es:" <<sumar() <<endl;
+	cout <<"Multiplicacion de los numeros es:" <<multiplicar()<<endl;
+	return 0;
+}
+
